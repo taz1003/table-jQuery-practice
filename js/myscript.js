3d0cc14ce5d040948bddedff70a739022537bd76
taz1003 <li9vyrit@students.codeinstitute.net>
2025-04-11 20:58:35 +0200

    Add 2 new classes to style

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	// $('h2').css('text-decoration', 'underline');
+	$("h2").addClass("underline");
+	// $('ul').css('border', 'solid 1px #ccc');
+	$("ul").addClass("border");
+});
