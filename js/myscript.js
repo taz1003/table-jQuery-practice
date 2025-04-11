54e8c6a1f3de4d73bfca9ef3220258cb9ccc5ba1
taz1003 <li9vyrit@students.codeinstitute.net>
2025-04-11 21:05:14 +0200

    Learn to remove class from jQuery

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	// $('h2').css('text-decoration', 'underline');
-	$("h2").addClass("underline");
-	// $('ul').css('border', 'solid 1px #ccc');
-	$("ul").addClass("border");
+	// // $('h2').css('text-decoration', 'underline');
+	// $("h2").addClass("underline"); // Add underline to h2 elements
+	// // $('ul').css('border', 'solid 1px #ccc');
+	// $("ul").removeClass("border");  // Remove border from ul elements
 });
